c0d79c1e84631f791bfea9e6bf5db469744a9bfa
pHervasC <phervascalvet@gmail.com>
2024-10-22 15:48:56 +0200

    Fin

--- src/app/component/usuario/admin/plist/routed/usuario.admin.remove.routed/usuario.admin.remove.routed.component.ts
+++ src/app/component/usuario/admin/plist/routed/usuario.admin.remove.routed/usuario.admin.remove.routed.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../../../../../service/usuario.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IUsuario } from '../../../../../../model/usuario.interface';
 
 
 @Component({
   selector: 'app-usuario.admin.remove.routed',
   templateUrl: './usuario.admin.remove.routed.component.html',
-  styleUrls: ['./usuario.admin.remove.routed.component.css']
+  styleUrls: ['./usuario.admin.remove.routed.component.css'],
+  standalone: true,
+  imports: [],
 })
 export class UsuarioAdminRemoveRoutedComponent implements OnInit {
   userId: number = 0;
-
+  usuario: IUsuario = {
+    id: 0,
+    nombre: '',
+    apellido1: '',
+    apellido2: '',
+    email: '',
+  };
+  
 
   constructor(
     private route: ActivatedRoute, // Inyectamos ActivatedRoute
@@ -21,13 +31,26 @@ export class UsuarioAdminRemoveRoutedComponent implements OnInit {
   ngOnInit(): void {
     // Obtenemos el ID del usuario desde la URL
     this.userId = Number(this.route.snapshot.paramMap.get('id'));
+    this.getUser();
   }
 
   removeUser() {
     this.oUsuarioService.removeUser(this.userId).subscribe({
+
       next: (data) => {
         console.log(`Usuario con ID ${this.userId} eliminado.`);
         this.router.navigate(['/admin/usuario/plist']);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  getUser() {
+    this.oUsuarioService.get(this.userId).subscribe({
+      next: (data) => {
+        this.usuario = data;
       },
       error: (error) => {
         console.log(error);
